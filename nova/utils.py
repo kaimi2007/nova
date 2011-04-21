f703e9b08af60b373a2757715041595610ec455e
Mikyung Kang <mkkang@bespin>
2011-04-21 14:13:20 -0400

    Updated utils.py for tilera support based on hpc-trunk 768

--- nova/utils.py
+++ nova/utils.py
@@ -709,6 +709,8 @@ def subset_dict(dict_, keys):
 
 def check_isinstance(obj, cls):
     """Checks that obj is of type cls, and lets PyLint infer types"""
+    if FLAGS.connection_type == 'tilera':
+        return obj 
     if isinstance(obj, cls):
         return obj
     raise Exception(_("Expected object of type: %s") % (str(cls)))
