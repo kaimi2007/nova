e22dc04f60b84ca3f9441992a3f598856222ccb0
Mikyung Kang <mkkang@bespin>
2011-04-21 15:24:04 -0400

    style check up for utils.py

--- nova/utils.py
+++ nova/utils.py
@@ -710,7 +710,7 @@ def subset_dict(dict_, keys):
 def check_isinstance(obj, cls):
     """Checks that obj is of type cls, and lets PyLint infer types"""
     if FLAGS.connection_type == 'tilera':
-        return obj 
+        return obj
     if isinstance(obj, cls):
         return obj
     raise Exception(_("Expected object of type: %s") % (str(cls)))
