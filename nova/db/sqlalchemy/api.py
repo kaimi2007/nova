dcacb7c20e19327a43ead7a39b5771a2fc8a919f
Lorin Hochstein <lorin@isi.edu>
2011-05-27 15:43:36 -0400

    Merged in revisions 1116-1124 of instance_type_metadata

--- nova/db/sqlalchemy/api.py
+++ nova/db/sqlalchemy/api.py
@@ -2481,7 +2481,22 @@ def console_get(context, console_id, instance_id=None):
 
 @require_admin_context
 def instance_type_create(_context, values):
+    """Create a new instance type. In order to pass in metadata, 
+    the values dict should contain a 'meta' key/value pair: 
+    
+    {'meta' : {'k1': 'v1', 'k2': 'v2', ...}}
+    
+    """
     try:
+        metadata = values.get('meta')
+        metadata_refs = []
+        if metadata:
+            for k, v in metadata.iteritems():
+                metadata_ref = models.InstanceTypeMetadata()
+                metadata_ref['key'] = k
+                metadata_ref['value'] = v
+                metadata_refs.append(metadata_ref)
+        values['meta'] = metadata_refs
         instance_type_ref = models.InstanceTypes()
         instance_type_ref.update(values)
         instance_type_ref.save()
@@ -2686,4 +2701,67 @@ def instance_metadata_update_or_create(context, instance_id, metadata):
                             "instance_id": instance_id,
                             "deleted": 0})
         #meta_ref.save(session=session)
+    return metadata
+
+####################
+
+@require_context
+def instance_type_metadata_get(context, instance_type_id):
+    session = get_session()
+
+    meta_results = session.query(models.InstanceTypeMetadata).\
+                    filter_by(instance_type_id=instance_type_id).\
+                    filter_by(deleted=False).\
+                    all()
+
+    meta_dict = {}
+    for i in meta_results:
+        meta_dict[i['key']] = i['value']
+    return meta_dict
+
+
+@require_context
+def instance_type_metadata_delete(context, instance_type_id, key):
+    session = get_session()
+    session.query(models.InstanceTypeMetadata).\
+        filter_by(instance_type_id=instance_type_id).\
+        filter_by(key=key).\
+        filter_by(deleted=False).\
+        update({'deleted': True,
+                'deleted_at': datetime.datetime.utcnow(),
+                'updated_at': literal_column('updated_at')})
+
+
+@require_context
+def instance_type_metadata_get_item(context, instance_type_id, key):
+    session = get_session()
+
+    meta_result = session.query(models.InstanceMetadata).\
+                    filter_by(instance_type_id=instance_type_id).\
+                    filter_by(key=key).\
+                    filter_by(deleted=False).\
+                    first()
+
+    if not meta_result:
+        raise exception.InstanceTypeMetadataNotFound(metadata_key=key,
+                                                 instance_type_id=instance_type_id)
+    return meta_result
+
+
+@require_context
+def instance_type_metadata_update_or_create(context, instance_type_id, 
+                                            metadata):
+    session = get_session()
+    meta_ref = None
+    for key, value in metadata.iteritems():
+        try:
+            meta_ref = instance_type_metadata_get_item(context, 
+                                                       instance_type_id, key,
+                                                       session)
+        except:
+            meta_ref = models.InstanceTypeMetadata()
+        meta_ref.update({"key": key, "value": value,
+                            "instance_type_id": instance_type_id,
+                            "deleted": 0})
+        meta_ref.save(session=session)
     return metadata
--- nova/tests/test_instance_types_metadata.py
+++ nova/tests/test_instance_types_metadata.py
@@ -16,7 +16,8 @@
 Unit Tests for instance types metadata code
 """
 
-
+from nova import context
+from nova import db
 from nova import test
 from nova.db.sqlalchemy.session import get_session
 from nova.db.sqlalchemy import models
@@ -26,37 +27,80 @@ class InstanceTypeMetadataTestCase(test.TestCase):
     
     def setUp(self):
         super(InstanceTypeMetadataTestCase, self).setUp()
-        values = dict(memory_mb=22000,
+        self.context = context.get_admin_context()
+        values = dict(name="cg1.4xlarge",
+                      memory_mb=22000,
                       vcpus=8,
                       local_gb=1690,
                       flavorid=105)
         metadata = dict(cpu_arch="x86_64",
-                        cpu_info=dict(model="Nehalem"),
+                        cpu_model="Nehalem",
                         xpu_arch="fermi",
                         xpus=2,
-                        xpu_info=dict(model="Tesla 2050", gcores="448"),
-                        net_arch="ethernet",
-                        net_mbps=10000)
+                        xpu_model="Tesla 2050")
+        values['meta'] = metadata
+        ref = db.api.instance_type_create(self.context,
+                                          values)
+        self.instance_type_id = ref.id
         
-        metadata_refs = []
-        for k,v in metadata.iteritems():
-            metadata_ref = models.InstanceTypeMetadata()
-            metadata_ref['key'] = k
-            metadata_ref['value'] = v
-            metadata_refs.append(metadata_ref)
-        values['metadata'] = metadata_refs
-
-        instance_type_ref = models.InstanceTypes()
-        instance_type_ref.update(values)
-            
-        session = get_session()
-        with session.begin():
-            instance_type_ref.save(session=session)
-        # Add cg1.4xlarge
+    def tearDown(self):
+        # Remove the instance type from the database
+        db.api.instance_type_purge(context.get_admin_context(), "cg1.4xlarge")
+        super(InstanceTypeMetadataTestCase, self).tearDown()
         
-
-    def test_foo(self):
-        # Add a new instance type cg1
-        # Add the metadata
-        # Retrieve the metadata
-        pass
+    def test_instance_type_metadata_get(self):
+        expected_metadata = dict(cpu_arch="x86_64",
+                                 cpu_model="Nehalem",
+                                 xpu_arch="fermi",
+                                 xpus="2",
+                                 xpu_model="Tesla 2050")
+        actual_metadata = db.api.instance_type_metadata_get(
+                              context.get_admin_context(),
+                              self.instance_type_id)
+        self.assertEquals(expected_metadata, actual_metadata)
+        
+    def test_instance_type_metadata_delete(self):
+        expected_metadata = dict(cpu_arch="x86_64",
+                                 cpu_model="Nehalem",
+                                 xpu_arch="fermi",
+                                 xpus="2")
+        db.api.instance_type_metadata_delete(context.get_admin_context(), 
+                                      self.instance_type_id,
+                                      "xpu_model")
+        actual_metadata = db.api.instance_type_metadata_get(
+                              context.get_admin_context(),
+                              self.instance_type_id)
+        self.assertEquals(expected_metadata, actual_metadata)
+        
+    def test_instance_type_metadata_update(self):
+        expected_metadata = dict(cpu_arch="x86_64",
+                                 cpu_model="Sandy Bridge",
+                                 xpu_arch="fermi",
+                                 xpus="2",
+                                 xpu_model="Tesla 2050")
+        db.api.instance_type_metadata_update_or_create(
+                              context.get_admin_context(),
+                              self.instance_type_id,
+                              dict(cpu_model="Sandy Bridge"))
+        actual_metadata = db.api.instance_type_metadata_get(
+                              context.get_admin_context(),
+                              self.instance_type_id)
+        self.assertEquals(expected_metadata, actual_metadata)
+    
+    def test_instance_type_metadata_create(self):
+        expected_metadata = dict(cpu_arch="x86_64",
+                                 cpu_model="Nehalem",
+                                 xpu_arch="fermi",
+                                 xpus="2",
+                                 xpu_model="Tesla 2050", 
+                                 net_arch="ethernet",
+                                 net_mbps="10000")
+        db.api.instance_type_metadata_update_or_create(
+                              context.get_admin_context(),
+                              self.instance_type_id,
+                              dict(net_arch="ethernet",
+                                   net_mbps=10000))
+        actual_metadata = db.api.instance_type_metadata_get(
+                              context.get_admin_context(),
+                              self.instance_type_id)
+        self.assertEquals(expected_metadata, actual_metadata)
