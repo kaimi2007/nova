70ddfe8bf91030d36bb7c9a5388b8ef0a3a96e78
Lorin Hochstein <lorin@isi.edu>
2011-06-27 08:53:41 -0400

    pep8

--- nova/tests/test_hetero_scheduler.py
+++ nova/tests/test_hetero_scheduler.py
@@ -27,16 +27,18 @@ from nova import rpc
 from nova.scheduler import driver
 from nova.scheduler import manager
 
+
 class HeteroSchedulerTestCase(test.TestCase):
     """Test case for heterogeneous scheduler"""
+
     def setUp(self):
         super(HeteroSchedulerTestCase, self).setUp()
         driver = 'nova.scheduler.hetero.HeterogeneousScheduler'
         self.context = context.get_admin_context()
-        self.flags(scheduler_driver = driver)
+        self.flags(scheduler_driver=driver)
         specs = dict(cpu_arch="x86_64",
                      xpu_arch="fermi",
-                     xpu_model="Tesla S2050")                
+                     xpu_model="Tesla S2050")
         self.instance_type = dict(vcpus=1,
                                   memory_mb=10,
                                   local_gb=2,
@@ -55,26 +57,25 @@ class HeteroSchedulerTestCase(test.TestCase):
                         memory_mb=10,
                         local_gb=2,
                         instance_type_id=ref.id)
-        self.inst_ref = db.instance_create(self.context, instance)        
+        self.inst_ref = db.instance_create(self.context, instance)
 
     def tearDown(self):
         super(HeteroSchedulerTestCase, self).tearDown()
 
-
     def test_no_hosts(self):
-        scheduler = manager.SchedulerManager()  
-        self.mox.StubOutWithMock(rpc, 'cast', use_mock_anything = True)        
+        scheduler = manager.SchedulerManager()
+        self.mox.StubOutWithMock(rpc, 'cast', use_mock_anything=True)
         self.mox.ReplayAll()
         self.assertRaises(driver.NoValidHost,
                           scheduler.run_instance,
                           context=self.context,
-                          topic = 'compute',
-                          instance_id = self.inst_ref.id,
+                          topic='compute',
+                          instance_id=self.inst_ref.id,
                           request_spec={'instance_type': self.instance_type,
                                         'num_instances': 1})
-    
+
     def test_one_host_no_match_cap(self):
-        scheduler = manager.SchedulerManager()  
+        scheduler = manager.SchedulerManager()
         caps = {'vcpus': 16, 'memory_mb': 32, 'local_gb': 100,
                'vcpus_used': 1, 'local_gb_used': 10, 'host_memory_free': 21651,
                'host_memory_total': 23640, 'disk_total': 97, 'disk_used': 92,
@@ -83,21 +84,19 @@ class HeteroSchedulerTestCase(test.TestCase):
         scheduler.zone_manager.update_service_capabilities("compute",
                                                            "host1",
                                                            caps)
-        self.mox.StubOutWithMock(rpc, 'cast', use_mock_anything = True)
+        self.mox.StubOutWithMock(rpc, 'cast', use_mock_anything=True)
         self.mox.ReplayAll()
-        
 
         self.assertRaises(driver.NoValidHost,
                           scheduler.run_instance,
                           context=self.context,
-                          topic = 'compute',
-                          instance_id = self.inst_ref.id,
+                          topic='compute',
+                          instance_id=self.inst_ref.id,
                           request_spec={'instance_type': self.instance_type,
                                         'num_instances': 1})
 
-    
     def test_one_host_match_cap(self):
-        scheduler = manager.SchedulerManager()  
+        scheduler = manager.SchedulerManager()
         caps = {'vcpus': 16, 'memory_mb': 32, 'local_gb': 100,
                'vcpus_used': 1, 'local_gb_used': 10, 'host_memory_free': 21651,
                'host_memory_total': 23640, 'disk_total': 97, 'disk_used': 92,
@@ -111,21 +110,20 @@ class HeteroSchedulerTestCase(test.TestCase):
                                                            caps)
         request_spec = {'instance_type': self.instance_type,
                         'num_instances': 1}
-        self.mox.StubOutWithMock(rpc, 'cast', use_mock_anything = True)
+        self.mox.StubOutWithMock(rpc, 'cast', use_mock_anything=True)
         rpc.cast(self.context,
                  'compute.host1',
                  {'method': 'run_instance',
                   'args': {'instance_id': self.inst_ref.id,
-                           'request_spec' : request_spec}})
+                           'request_spec': request_spec}})
         self.mox.ReplayAll()
         scheduler.run_instance(self.context,
-                           topic = 'compute',
-                           instance_id = self.inst_ref.id,
-                           request_spec= request_spec)
+                           topic='compute',
+                           instance_id=self.inst_ref.id,
+                           request_spec=request_spec)
 
-    
     def test_two_host_one_match_cap(self):
-        scheduler = manager.SchedulerManager()  
+        scheduler = manager.SchedulerManager()
         caps_host1 = {'vcpus': 16, 'memory_mb': 32, 'local_gb': 100,
                'vcpus_used': 1, 'local_gb_used': 10, 'host_memory_free': 21651,
                'host_memory_total': 23640, 'disk_total': 97, 'disk_used': 92,
@@ -148,7 +146,7 @@ class HeteroSchedulerTestCase(test.TestCase):
         scheduler.zone_manager.update_service_capabilities("compute",
                                                            "host2",
                                                            caps_host2)
-        self.mox.StubOutWithMock(rpc, 'cast', use_mock_anything = True)
+        self.mox.StubOutWithMock(rpc, 'cast', use_mock_anything=True)
         rpc.cast(self.context,
                  'compute.host2',
                  {'method': 'run_instance',
@@ -156,12 +154,12 @@ class HeteroSchedulerTestCase(test.TestCase):
                             'request_spec': request_spec}})
         self.mox.ReplayAll()
         scheduler.run_instance(self.context,
-                               topic = 'compute',
-                               instance_id = self.inst_ref.id,
-                               request_spec = request_spec)
-    
+                               topic='compute',
+                               instance_id=self.inst_ref.id,
+                               request_spec=request_spec)
+
     def two_host_two_match_cap(self):
-        scheduler = manager.SchedulerManager()  
+        scheduler = manager.SchedulerManager()
         caps_host1 = {'vcpus': 16, 'memory_mb': 32, 'local_gb': 100,
                'vcpus_used': 1, 'local_gb_used': 10, 'host_memory_free': 21651,
                'disk_available': 5,
@@ -186,8 +184,8 @@ class HeteroSchedulerTestCase(test.TestCase):
         scheduler.zone_manager.update_service_capabilities("compute",
                                                            "host2",
                                                            caps_host2)
-        self.mox.StubOutWithMock(rpc, 'cast', use_mock_anything = True)
-        self.mox.StubOutWithMock(random, 'random', use_mock_anything = True)
+        self.mox.StubOutWithMock(rpc, 'cast', use_mock_anything=True)
+        self.mox.StubOutWithMock(random, 'random', use_mock_anything=True)
         random.random().AndReturn(0)
         rpc.cast(self.context,
                  'compute.host2',
@@ -196,6 +194,6 @@ class HeteroSchedulerTestCase(test.TestCase):
                            'request_spec': request_spec}})
         self.mox.ReplayAll()
         scheduler.run_instance(self.context,
-                               topic = 'compute',
-                               instance_id = self.inst_ref.id,
-                               request_spec = request_spec)
+                               topic='compute',
+                               instance_id=self.inst_ref.id,
+                               request_spec=request_spec)
