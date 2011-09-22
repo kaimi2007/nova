2afd4b3710376070e79340d807750b4ceec42b2d
jsuh <jsuh@bespin>
2011-09-22 13:23:23 -0400

    populate instance_type_extra_specs

--- nova/db/sqlalchemy/migrate_repo/versions/024_add_instance_type_extra_specs.py
+++ nova/db/sqlalchemy/migrate_repo/versions/024_add_instance_type_extra_specs.py
@@ -48,6 +48,7 @@ instance_type_extra_specs_table = Table('instance_type_extra_specs', meta,
                       unicode_error=None, _warn_on_bytestring=False)))
 
 
+
 def upgrade(migrate_engine):
     # Upgrade operations go here. Don't create your own engine;
     # bind migrate_engine to your metadata
@@ -60,8 +61,50 @@ def upgrade(migrate_engine):
             logging.exception('Exception while creating table')
             raise
 
+    try:
+        i = instance_type_extra_specs_table.insert()
+        for name, values in INSTANCE_TYPE_EXTRA_SPECS.iteritems():
+            i.execute({'deleted': 0,
+                       'instance_type_id': values["instance_type_id"],
+                       'key': values["key"],
+                       'value': values["value"]
+                       })
+    except Exception:
+        logging.info(repr(instance_types))
+        logging.exception('Exception while seeding instance_type_extra_specs table')
+        raise
 
 def downgrade(migrate_engine):
     # Operations to reverse the above upgrade go here.
     for table in (instance_type_extra_specs_table, ):
         table.drop()
+
+
+INSTANCE_TYPE_EXTRA_SPECS = {
+
+    # x86+GPU
+    # TODO: we need to identify machine readable string for xpu arch
+    '1': dict(instance_type_id=26, key='xpu_arch', value='fermi'),
+    '2': dict(instance_type_id=7, key='xpu_arch', value='fermi'),
+    '3': dict(instance_type_id=14, key='xpu_arch', value='fermi'),
+    '4': dict(instance_type_id=15, key='xpu_arch', value='fermi'),
+    '5': dict(instance_type_id=19, key='xpu_arch', value='fermi'),
+    '6': dict(instance_type_id=8, key='xpu_arch', value='fermi'),
+
+    '10': dict(instance_type_id=11, key='cpu_arch', value='x86_64'),
+    '11': dict(instance_type_id=23, key='cpu_arch', value='x86_64'),
+    '12': dict(instance_type_id=9, key='cpu_arch', value='x86_64'),
+    '13': dict(instance_type_id=24, key='cpu_arch', value='x86_64'),
+    '14': dict(instance_type_id=10, key='cpu_arch', value='x86_64'),
+    '15': dict(instance_type_id=18, key='cpu_arch', value='x86_64'),
+    '16': dict(instance_type_id=21, key='cpu_arch', value='x86_64'),
+    '17': dict(instance_type_id=13, key='cpu_arch', value='x86_64'),
+    '18': dict(instance_type_id=25, key='cpu_arch', value='x86_64'),
+    '19': dict(instance_type_id=12, key='cpu_arch', value='x86_64'),
+
+    '100': dict(instance_type_id=6, key='cpu_arch', value='tilepro64'),
+    '101': dict(instance_type_id=16, key='cpu_arch', value='tilepro64'),
+    '102': dict(instance_type_id=20, key='cpu_arch', value='tilepro64'),
+    '103': dict(instance_type_id=17, key='cpu_arch', value='tilepro64'),
+    '104': dict(instance_type_id=22, key='cpu_arch', value='tilepro64')
+    }
