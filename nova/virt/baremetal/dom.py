b5bc82d6b0d9003c475455212196aa1959a5147f
Lorin Hochstein <lorin@isi.edu>
2011-10-19 16:31:39 -0400

    Added comments and headers
    Added comments and headers to some files to start preparing for proposing upstream
    
    Change-Id: I085c57093acda812295911fe6a9e3db2aeacc6a1

--- nova/virt/baremetal/dom.py
+++ nova/virt/baremetal/dom.py
@@ -1,3 +1,19 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright (c) 2011 University of Southern California
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 import multiprocessing
 import os
 import random
@@ -7,7 +23,6 @@ import sys
 import tempfile
 import time
 import uuid
-import pickle
 from xml.dom import minidom
 from xml.etree import ElementTree
 
@@ -84,7 +99,7 @@ class BareMetalDom(object):
 
     def _read_domain_from_file(self):
         """
-        Reads the domains from a pickled representation.
+        Reads the domains from a file.
         """
         try:
             self.domains = read_domains(self.fake_dom_file)
--- nova/virt/baremetal/tilera.py
+++ nova/virt/baremetal/tilera.py
@@ -1,3 +1,26 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright (c) 2011 University of Southern California
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""
+Tilera back-end for bare-metal compute node provisioning
+
+The details of this implementation are specific to ISI's testbed. This code
+is provided here as an example of how to implement a backend.
+"""
+
 import base64
 import multiprocessing
 import os
