c06959e64cde92a611eae69019255a580e0cf829
Lorin Hochstein <lorin@isi.edu>
2011-10-19 11:04:13 -0400

    Added domain read/write tests.
    Added tests for reading/writing domains to file.

--- nova/tests/baremetal/test_proxy_bare_metal.py
+++ nova/tests/baremetal/test_proxy_bare_metal.py
@@ -15,13 +15,13 @@
 
 import __builtin__
 
+import functools
 import mox
-
-import pickle
 import StringIO
 import stubout
 
 from nova import flags
+from nova import utils
 from nova import test
 from nova.compute import power_state
 from nova import context
@@ -41,6 +41,94 @@ fake_domains = [{'status': 1, 'name': 'instance-00000001',
                  'vcpus': 1, 'node_id': 6,
                  'mac_address': '02:16:3e:01:4e:c9',
                  'ip_address': '10.5.1.2'}]
+
+def json_equal(x, y):
+    """Check if two json strings represent the equivalent Python object"""
+    return utils.loads(x)==utils.loads(y)
+
+
+class DomainReadWriteTestCase(test.TestCase):
+
+    def test_read_domain_with_empty_list(self):
+        """Read a file that contains no domains"""
+
+        self.mox.StubOutWithMock(__builtin__, 'open')
+        try:
+            fake_file = StringIO.StringIO('[]')
+            open('/tftpboot/test_fake_dom_file', 'r').AndReturn(fake_file)
+
+            self.mox.ReplayAll()
+
+            domains = dom.read_domains('/tftpboot/test_fake_dom_file')
+
+            self.assertEqual(domains, [])
+
+        finally:
+            self.mox.UnsetStubs()
+
+    def test_read_domain(self):
+        """Read a file that contains at least one domain"""
+        fake_file = StringIO.StringIO('''[{"status": 1, 
+         "image_id": "1552326678", "vcpus": 1, "node_id": 6, 
+         "name": "instance-00000001", "memory_kb": 16777216, 
+         "mac_address": "02:16:3e:01:4e:c9", "kernel_id": "1896115634", 
+         "ramdisk_id": "", "ip_address": "10.5.1.2"}]''')
+
+        self.mox.StubOutWithMock(__builtin__, 'open')
+        try:
+            open('/tftpboot/test_fake_dom_file', 'r').AndReturn(fake_file)
+
+            self.mox.ReplayAll()
+
+            domains = dom.read_domains('/tftpboot/test_fake_dom_file')
+
+            self.assertEqual(domains, fake_domains)
+
+        finally:
+            self.mox.UnsetStubs()
+
+    def test_read_no_file(self):
+        """Try to read when the file does not exist
+
+        This should through and IO exception"""
+
+        self.mox.StubOutWithMock(__builtin__, 'open')
+        try:
+            open('/tftpboot/test_fake_dom_file', 'r').AndRaise(\
+            IOError(2, 'No such file or directory', '/tftpboot/test_fake_dom_file'))
+
+            self.mox.ReplayAll()
+
+            self.assertRaises(IOError, dom.read_domains, '/tftpboot/test_fake_dom_file')
+                    
+        finally:
+            self.mox.UnsetStubs()
+
+    def test_write_domain(self):
+        """Write the domain to file"""
+        self.mox.StubOutWithMock(__builtin__, 'open')
+        mock_file = self.mox.CreateMock(file)
+        expected_json = '''[{"status": 1, 
+               "image_id": "1552326678", "vcpus": 1, "node_id": 6, 
+               "name": "instance-00000001", "memory_kb": 16777216, 
+               "mac_address": "02:16:3e:01:4e:c9", "kernel_id": "1896115634", 
+               "ramdisk_id": "", "ip_address": "10.5.1.2"}]'''
+        try:
+            open('/tftpboot/test_fake_dom_file', 'w').AndReturn(mock_file)
+
+            # Check if the argument to file.write() represents the same 
+            # Python object as expected_json
+            # We can't do an exact string comparison because of ordering and whitespace
+            mock_file.write(mox.Func(functools.partial(json_equal, expected_json)))
+            mock_file.close()
+
+            self.mox.ReplayAll()
+
+            dom.write_domains('/tftpboot/test_fake_dom_file', fake_domains)
+
+        
+        finally:
+            self.mox.UnsetStubs()
 
 
 class BareMetalDomTestCase(test.TestCase):
