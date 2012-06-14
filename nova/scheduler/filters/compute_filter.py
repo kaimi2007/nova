60446bad9d01c1ce32db82b12c21f278f5eca1db
Dong-In David Kang <dkang@isi.edu>
2012-06-14 15:00:02 -0400

    upstream/stable/esstx June 14th merge + scheduler debug

--- nova/scheduler/filters/compute_filter.py
+++ nova/scheduler/filters/compute_filter.py
@@ -13,6 +13,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import operator
+
 from nova import log as logging
 from nova.scheduler import filters
 from nova import utils
