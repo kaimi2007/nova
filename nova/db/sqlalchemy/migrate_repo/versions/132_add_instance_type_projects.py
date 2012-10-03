e50f1b5b2bfcf916632cebbc0cefeadfd5a28fce
David Kang <dkang@isi.edu>
2012-10-03 14:41:51 -0400

    new instance types are added.

--- nova/db/sqlalchemy/migrate_repo/versions/132_add_instance_type_projects.py
+++ nova/db/sqlalchemy/migrate_repo/versions/132_add_instance_type_projects.py
@@ -15,9 +15,89 @@
 from sqlalchemy import Boolean, Column, DateTime, String, ForeignKey, Integer
 from sqlalchemy import MetaData, String, Table
 
+from nova import context
+from nova import db
+
 from nova.openstack.common import log as logging
 
 LOG = logging.getLogger(__name__)
+
+def _populate_instance_type_extra_specs(instance_types):
+    try:
+        instance_type_rows = list(instance_types.select().execute())
+        for instance_type in instance_type_rows:
+            flavorid = instance_type.flavorid
+            name = instance_type.name
+            if (name == 'm1.tiny') or \
+                   (name == 'm1.small') or \
+                   (name == 'm1.medium') or \
+                   (name == 'm1.large') or \
+                   (name == 'm1.xlarge'):
+                    extra_specs = dict(cpu_arch='s== x86_64',
+                                       hypervisor_type='s== QEMU')
+            elif (name == 'cg1.small'):
+                    extra_specs = dict(
+                                      cpu_arch='s== x86_64',
+                                      gpu_arch='s== fermi',
+                                      gpus='= 1',
+                                      hypervisor_type='s== LXC')
+            elif (name == 'cg1.medium'):
+                    extra_specs = dict(
+                                      cpu_arch='s== x86_64',
+                                      gpu_arch='s== fermi',
+                                      gpus='= 2',
+                                      hypervisor_type='s== LXC')
+            elif (name == 'cg1.large'):
+                    extra_specs = dict(
+                                      cpu_arch='s== x86_64',
+                                      gpu_arch='s== fermi',
+                                      gpus='= 3',
+                                      hypervisor_type='s== LXC')
+            elif (name == 'cg1.xlarge'):
+                    extra_specs = dict(
+                                      cpu_arch='s== x86_64',
+                                      gpu_arch='s== fermi',
+                                      gpus='= 4',
+                                      hypervisor_type='s== LXC')
+            elif (name == 'cg1.2xlarge'):
+                    extra_specs = dict(
+                                      cpu_arch='s== x86_64',
+                                      gpu_arch='s== fermi',
+                                      gpus='= 4',
+                                      hypervisor_type='s== LXC')
+            elif (name == 'cg1.4xlarge'):
+                    extra_specs = dict(
+                                      cpu_arch='s== x86_64',
+                                      gpu_arch='s== fermi',
+                                      gpus='= 4',
+                                      hypervisor_type='s== LXC')
+            elif (name == 'sh1.small') or  \
+                     (name == 'sh1.medium') or \
+                     (name == 'sh1.large') or \
+                     (name == 'sh1.xlarge') or \
+                     (name == 'sh1.2xlarge') or \
+                     (name == 'sh1.4xlarge') or \
+                     (name == 'sh1.8xlarge') or \
+                     (name == 'sh1.16xlarge') or \
+                     (name == 'sh1.32xlarge'):
+                    extra_specs = dict(
+                                      cpu_arch='s== x86_64',
+                                      system_type='s== UV',
+                                      hypervisor_type='s== QEMU')
+            elif (name == 'tp64.8x8'):
+                    extra_specs = dict(
+                                      cpu_arch='s== tilepro64',
+                                      hypervisor_type='s== tilera_hv',
+                                      vcores='=64')
+
+            db.instance_type_extra_specs_update_or_create(
+                                          context.get_admin_context(),
+                                          flavorid,
+                                          extra_specs)
+
+    except Exception:
+        LOG.exception('Exception while creating extra_specs table')
+        raise
 
 
 def upgrade(migrate_engine):
@@ -51,6 +131,8 @@ def upgrade(migrate_engine):
     except Exception:
         LOG.error(_("Table |%s| not created!"), repr(instance_type_projects))
         raise
+
+    _populate_instance_type_extra_specs(instance_types)
 
 
 def downgrade(migrate_engine):
