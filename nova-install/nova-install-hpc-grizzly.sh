b08e3d2fd19f68e2ff84cbedf0054198f1703dc2
David Kang <dkang@isi.edu>
2013-05-17 16:25:28 -0400

    nova.conf for different architectures are debugged.

--- nova-install/nova-install-hpc-grizzly.sh
+++ nova-install/nova-install-hpc-grizzly.sh
@@ -55,7 +55,9 @@ ARCH=$2
 MYSQL_ROOT_USR=root
 MYSQL_ROOT_PASS=nova
 MYSQL_NOVA_USR=nova
-MYSQL_NOVA_PASS=sqlnova
+MYSQL_NOVA_PASS=nova
+Keystone_User_Nova=nova
+Keystone_Password_Nova=secrete
 NET_MAN=FlatDHCPManager
 BRIDGE=br100
 NETWORK_LABEL=public
@@ -64,24 +66,21 @@ NOVA_CONF=/etc/nova/nova.conf
 NETWORK_SIZE=256
 USER=/home/nova
 CGROUPS_PATH=/cgroup/devices/libvirt/lxc
-# We assume that IP address of br100 of cloud controller is 10.99.0.1
+# We assume that IP address of eth0 (PUBLIC_INTERFACE) of cloud controller is 10.1.1.4
+# We assume that the bridge br100 is associated with eth2 (FLAT_INTERFACE).
 # The followings should be changed if your network settings are different from this.
-# Arbitrary value of DHCP_START_IP other than x.x.x.2 doesn't seem to work as of this release.
-HOST_IP=10.99.0.1
-NOVA_API_server_IP_address=10.99.0.1
-Glance_server_IP_address=10.99.0.1
-Volume_server_IP_address=10.99.0.1
-Keystone_server_IP_address=10.99.0.1
-MySQL_Nova_IP_address=10.99.0.1
-DHCP_FIXED_RANGE=10.99.0.0/24
-DHCP_START_IP=10.99.0.2
+NOVA_API_server_IP_address=10.1.1.4
+Rabbitmq_IP_address=$NOVA_API_server_IP_address
+Glance_server_IP_address=$NOVA_API_server_IP_address
+Volume_server_IP_address=$NOVA_API_server_IP_address
+Keystone_server_IP_address=$NOVA_API_server_IP_address
+MySQL_Nova_IP_address=$NOVA_API_server_IP_address
+METADATA_HOST=$NOVA_API_server_IP_address
+DHCP_FIXED_RANGE=10.111.0.0/24
 DHCP_IP_NUM=$NETWORK_SIZE
-FLAT_NETWORK_DNS=10.99.0.1
-META_DATA_HOST=$NOVA_API_server_IP_address
 
 PUBLIC_INTERFACE=eth0
-FLAT_INTERFACE=eth1
-VLAN_INTERFACE=$FLAT_INTERFACE
+FLAT_INTERFACE=eth2
 
 API_PASTE_INI=/etc/nova/api-paste.ini
 QUOTA_CORES=1024
@@ -91,53 +90,12 @@ QUOTA_VOLUMES=100
 PERIODIC_INTERVAL=20
 DOWN_TIME=120
 # system specific info: The followings are initialized per architecture. Default value is eth0.
-BRIDGE_IFACE=eth1
+BRIDGE_IFACE=$FLAT_INTERFACE
 BAREMETAL_DRIVER=
-LIBVIRT_TYPE=
-CPU_ARCH=
-CONNECTION_TYPE=
-GPU_ARCH=
-GPUS=
+GPU_ARCH=fermi
+GPUS=4
 MAX_NBD_DEVICES=16
-if [ "$ARCH" == "gpu" ]; then
-    LIBVIRT_TYPE=lxc
-    CPU_ARCH=x86_64
-    CONNECTION_TYPE=gpu
-    GPU_ARCH=fermi
-    GPUS=4
-    EXTRA_SPECS="cpu_arch:$CPU_ARCH, gpus:$GPUS, gpu_arch:$GPU_ARCH"
-    COMPUTE_DRIVER=gpu.GPULibvirtDriver
-elif [ "$ARCH" == "tilera" ]; then
-    BAREMETAL_DRIVER=tilera
-    LIBVIRT_TYPE=kvm
-    CPU_ARCH=tilepro64
-    CONNECTION_TYPE=baremetal
-    EXTRA_SPECS="cpu_arch:$CPU_ARCH"
-    GPU_ARCH=
-    COMPUTE_DRIVER=baremetal.BareMetalDriver
-elif [ "$ARCH" == "uv" ]; then
-    LIBVIRT_TYPE=kvm
-    CPU_ARCH=x86_64
-    CONNECTION_TYPE=libvirt
-    COMPUTE_DRIVER=libvirt.LibvirtDriver
-    EXTRA_SPECS="cpu_arch:$CPU_ARCH, system_type:UV"
-else
-    LIBVIRT_TYPE=kvm
-    CPU_ARCH=x86_64
-    CONNECTION_TYPE=libvirt
-    COMPUTE_DRIVER=gpu.GPULibvirtDriver
-    EXTRA_SPECS="cpu_arch:$CPU_ARCH"
-fi
 
-# HOST_IP should be set at the IP address of br100 of the cloud controller.
-# For the datacenter machines, we use 10.1.1.1 for HOST_IP.
-if [ ! -n "$HOST_IP" ]; then
-    # NOTE(vish): This will just get the first ip in the list, so if you
-    #             have more than one eth device set up, this will fail, and
-    #             you should explicitly set HOST_IP in your environment
-    HOST_IP=`ifconfig  | grep -m 1 'inet addr:'| cut -d: -f2 | awk '{print $1}'`
-fi
-echo "Host IP = " $HOST_IP
 echo "Cloud Host IP = " ${NOVA_API_server_IP_address}
 MYSQL_ROOT_PASS=${MYSQL_ROOT_PASS:-nova}
 # NOTE(vish): If you are using FlatDHCP on multiple hosts, set the interface
@@ -168,8 +126,12 @@ dhcpbridge_flagfile = /etc/nova/nova.conf
 force_dhcp_release = False
 network_manager = nova.network.manager.FlatDHCPManager
 iscsi_helper = tgtadm
-sql_connection = mysql://nova:nova@localhost/nova
-rpc_backend = nova.openstack.common.rpc.impl_qpid
+rpc_backend = nova.rpc.impl_kombu
+rabbit_host=$Rabbitmq_IP_address
+ec2_host=$NOVA_API_server_IP_address
+ec2_dmz_host=$NOVA_API_server_IP_address
+glance_host=$Glance_server_IP_address
+iscsi_ip_address=$Volume_server_IP_address
 rootwrap_config = /etc/nova/rootwrap.conf
 volume_api_class = nova.volume.cinder.API
 enabled_apis = ec2,osapi_compute,metadata
@@ -189,17 +151,19 @@ novncproxy_port=6080
 novncproxy_host=$NOVA_API_server_IP_address
 NOVA_CONF_EOF
 fi
- 
+
 chown nova:nova $NOVA_CONF
 chmod 600  $NOVA_CONF
 if [ "$CMD" == "compute-init" ] ||
    [ "$CMD" == "single-init" ]; then
         if [ "$ARCH" == "gpu" ]; then
+		echo "sql_connection = mysql://$MYSQL_NOVA_USR:$MYSQL_NOVA_PASS@$MySQL_Nova_IP_address/nova"  >>  $NOVA_CONF
                 echo "user=$USER" >>  $NOVA_CONF
                 echo "use_cow_images=False" >>  $NOVA_CONF
 		echo "compute_driver = gpu.GPULibvirtDriver" >> $NOVA_CONF
 		echo "libvirt_type=lxc" >> $NOVA_CONF
                 echo "dev_cgroups_path=$CGROUPS_PATH"  >>  $NOVA_CONF
+		echo "instance_type_extra_specs = cpu_arch:x86_64, gpus:$GPUS, gpu_arch:$GPU_ARCH" >> $NOVA_CONF
         elif [ "$ARCH" == "tilera" ]; then
 		echo "libvirt_type=kvm" >> $NOVA_CONF
                 echo "tile_monitor=/usr/local/TileraMDE/bin/tile-monitor" >> $NOVA_CONF
@@ -210,13 +174,20 @@ if [ "$CMD" == "compute-init" ] ||
 		echo "reserved_host_memory_mb = 0" >> $NOVA_CONF
 		echo "" >> $NOVA_CONF
 		echo "[baremetal]" >> $NOVA_CONF
-		echo "net_config_template = /usr/lib/python2.6/site-packages/nova-2013.1.a4957.gf543f34-py2.6.egg/nova/virt/baremetal/net-static.ubuntu.template" >> $NOVA_CONF
+		echo "net_config_template = /usr/lib/python2.6/site-packages/nova/virt/baremetal/net-static.ubuntu.template"  >> $NOVA_CONF
 		echo "tftp_root = /tftpboot" >> $NOVA_CONF
 		echo "power_manager = nova.virt.baremetal.tilera_pdu.PDU" >> $NOVA_CONF
 		echo "driver = nova.virt.baremetal.tilera.TILERA" >> $NOVA_CONF
 		echo "instance_type_extra_specs = cpu_arch:tilepro64" >> $NOVA_CONF
 		echo "sql_connection = mysql://nova:nova@localhost/nova_bm" >> $NOVA_CONF
+        elif [ "$ARCH" == "uv" ]; then
+		echo "sql_connection = mysql://$MYSQL_NOVA_USR:$MYSQL_NOVA_PASS@$MySQL_Nova_IP_address/nova"  >>  $NOVA_CONF
+                echo "use_cow_images=True" >>  $NOVA_CONF
+		echo "compute_driver = libvirt.LibvirtDriver" >> $NOVA_CONF
+		echo "libvirt_type=kvm" >> $NOVA_CONF
+		echo "instance_type_extra_specs = cpu_arch:x86_64, system_type:UV" >> $NOVA_CONF
         else 
+		echo "sql_connection = mysql://$MYSQL_NOVA_USR:$MYSQL_NOVA_PASS@$MySQL_Nova_IP_address/nova"  >>  $NOVA_CONF
                 echo "use_cow_images=True" >>  $NOVA_CONF
 		echo "compute_driver = libvirt.LibvirtDriver" >> $NOVA_CONF
 		echo "libvirt_type=kvm" >> $NOVA_CONF
@@ -226,9 +197,9 @@ fi
 echo "" >> $NOVA_CONF
 echo "[keystone_authtoken]" >> $NOVA_CONF
 echo "admin_tenant_name = service" >> $NOVA_CONF
-echo "admin_user = nova" >> $NOVA_CONF
-echo "admin_password = nova" >> $NOVA_CONF
-echo "auth_host = 127.0.0.1" >> $NOVA_CONF
+echo "admin_user = $Keystone_User_Nova" >> $NOVA_CONF
+echo "admin_password = $Keystone_Password_Nova" >> $NOVA_CONF
+echo "auth_host = $Keystone_server_IP_address" >> $NOVA_CONF
 echo "auth_port = 35357" >> $NOVA_CONF
 echo "auth_protocol = http" >> $NOVA_CONF
 echo "signing_dir = /tmp/keystone-signing-nova" >> $NOVA_CONF
@@ -237,7 +208,7 @@ if [ "$CMD" == "compute-init" ] ||
    [ "$CMD" == "single-init" ]; then
     sudo killall libvirtd
     sudo modprobe kvm
-    sudo modprobe nbd
+#    sudo modprobe nbd
     service libvirtd restart
 fi
 if [ "$CMD" == "cloud-init" ] ||
@@ -256,10 +227,43 @@ if [ "$CMD" == "cloud-init" ] ||
     service openstack-nova-objectstore restart
     service openstack-nova-scheduler restart
     service openstack-nova-cert restart
+
+   
+    # create flavors for heterogeneous instance types 
+    nova flavor-key 10 set gpus="= 1"
+    nova flavor-key 10 set gpu_arch="s== fermi"
+
+    nova flavor-create --is-public True cg1.medium 11 4096 20 2
+    nova flavor-key 11 set gpus="= 1"
+    nova flavor-key 11 set gpu_arch="s== fermi"
+    nova flavor-create --is-public True cg1.large 12 8192 40 4
+    nova flavor-key 12 set gpus="= 1"
+    nova flavor-key 12 set gpu_arch="s== fermi"
+    nova flavor-create --is-public True cg1.xlarge 13 8192 80 8
+    nova flavor-key 13 set gpus="= 1"
+    nova flavor-key 13 set gpu_arch="s== fermi"
+    nova flavor-create --is-public True cg1.2xlarge 14 16384 160 8
+    nova flavor-key 14 set gpus="= 2"
+    nova flavor-key 14 set gpu_arch="s== fermi"
+    nova flavor-create --is-public True cg1.4xlarge 15 22000 320 8
+    nova flavor-key 15 set gpus="= 2"
+    nova flavor-key 15 set gpu_arch="s== fermi"
+
+    nova flavor-create --is-public True tp64.8x8 20 16218 917 64
+    nova flavor-key 20 set cpu_arch="s== tilepro64"
+
+    nova flavor-create --is-public True sh1.2xlarge 50 65536 160 16
+    nova flavor-key 50 set system_type="s== UV"
+    nova flavor-create --is-public True sh1.4xlarge 51 65536 160 32
+    nova flavor-key 51 set system_type="s== UV"
+    nova flavor-create --is-public True sh1.8xlarge 52 131072 160 64
+    nova flavor-key 52 set system_type="s== UV"
+
 fi
 if [ "$CMD" == "compute-init" ] ||
    [ "$CMD" == "single-init" ]; then
        echo "start openstack-nova-compute"
+      service openstack-nova-conductor restart
       service openstack-nova-compute restart
 fi
 
